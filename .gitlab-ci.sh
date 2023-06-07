f43d18867b3d983227ac438c44ac834f916c9d83
IvanSubbotin1 <103884993+IvanSubbotin1@users.noreply.github.com>
2023-06-07 12:48:40 +0300

    Create .gitlab-ci.sh

---
+++ .gitlab-ci.sh
@@ -0,0 +1,32 @@
+DOCKERFILES=""
+
+function search_all_dockerfiles_task() {
+    DOCKERFILES=$(find "dockerfiles" -name "*.Dockerfile" -type f)
+}
+
+function search_changed_dockerfiles_task() {
+    DOCKERFILES=$(git diff --name-only HEAD HEAD~1 -- 'dockerfiles/***.Dockerfile')
+}
+
+function install_git_task() {
+    # Вообще говоря, так не очень хорошо делать, но для примера можно...
+    apk update
+    apk add git
+}
+
+function build_and_push_dockerfiles_task() {
+    docker login "$CI_REGISTRY" \
+        --username "$CI_REGISTRY_USER" \
+        --password "$CI_REGISTRY_PASSWORD"
+
+    for dockerfile in $DOCKERFILES; do
+        path=$(echo "$dockerfile" | sed 's/^dockerfiles\///' | sed 's/\.Dockerfile$//')
+        tag="$CI_REGISTRY/$CI_PROJECT_PATH/$path:$CI_COMMIT_REF_SLUG"
+
+        echo "Building $dockerfile..."
+        docker build --file "$dockerfile" --tag "$tag" .
+
+        echo "Pushing $tag..."
+        docker push "$tag"
+    done
+}
